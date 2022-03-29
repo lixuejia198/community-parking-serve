a78e8cc9fc523b222e231a0e54ed5aa5ac1ee039
F999999999 <9811@88.com>
2022-03-29 21:29:43 +0800

    优化车位列表接口

--- model/carport.js
+++ model/carport.js
@@ -1,21 +1,23 @@
+const { query } = require("../db/query");
+
 // 根据用户Id查询车位信息
-module.exports.getCarportByUserID = async ({uid}) => {
+module.exports.getCarportByUserID = async ({ uid }) => {
   return await query(
-      "SELECT c.id,pname,state,comname,place,cid,comid,x,y,z FROM carport AS c,community AS com WHERE c.comid = com.id AND uid = ?",
-      [uid]
+    "SELECT c.id,pname,state,comname,place,cid,comid,x,y,z,direction FROM carport AS c,community AS com WHERE c.comid = com.id AND uid = ?",
+    [uid]
   );
 };
 // 根据小区Id查询车位信息
-module.exports.getCarportByComID = async ({comid}) => {
+module.exports.getCarportByComID = async ({ comid }) => {
   return await query(
-      "SELECT c.id,pname,state,comname,place,cid,comid,x,y,z FROM carport AS c,community AS com WHERE c.comid = com.id AND comid = ?",
-      [comid]
+    "SELECT c.id,pname,state,comname,place,cid,comid,x,y,z,direction FROM carport AS c,community AS com WHERE c.comid = com.id AND comid = ?",
+    [comid]
   );
 };
 // 根据用户Id与小区ID查询车位信息
-module.exports.getCarportByUserIDAndComID = async ({uid, comid}) => {
+module.exports.getCarportByUserIDAndComID = async ({ uid, comid }) => {
   return await query(
-      "SELECT c.id,pname,state,comname,place,cid,comid,x,y,z FROM carport AS c,community AS com WHERE c.comid = com.id AND uid = ? AND comid = ?",
-      [uid, comid]
+    "SELECT c.id,pname,state,comname,place,cid,comid,x,y,z,direction FROM carport AS c,community AS com WHERE c.comid = com.id AND uid = ? AND comid = ?",
+    [uid, comid]
   );
 };
