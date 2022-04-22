bcb8fc2e7aa95ce7bd711247f668bacaf12fbeb9
F999999999 <9811@88.com>
2022-04-22 20:01:37 +0800

    新增：车位操作相关接口

--- controller/carport.js
+++ controller/carport.js
@@ -2,6 +2,9 @@ const {
   getCarportByUserID,
   getCarportByComID,
   getCarportByUserIDAndComID,
+  addCarportByComID,
+  addCarportToUser,
+  addRentByCarport,
 } = require("../model/carport");
 
 // 查询用户车位列表
@@ -25,8 +28,7 @@ module.exports.getCarport = async (ctx) => {
     // 传入 uid 和 comid 则查询该用户指定小区的车位
     result = await getCarportByUserIDAndComID({ uid, comid });
   }
-  console.log(result, "result");
-  if (result.length > 0) {
+  if (result?.length > 0) {
     ctx.body = {
       status: 200,
       msg: "查询成功",
@@ -36,6 +38,92 @@ module.exports.getCarport = async (ctx) => {
     ctx.body = {
       status: 0,
       msg: "查询失败",
+    };
+  }
+};
+
+// 添加车位
+module.exports.addCarport = async (ctx) => {
+  const { pname, comid, x, y, z, direction, uid } = ctx.request.query;
+
+  // 添加小区车位
+  const result = await addCarportByComID({
+    pname,
+    comid: Number(comid),
+    x: Number(x),
+    y: Number(y),
+    z: Number(z),
+    direction: Number(direction),
+    uid: Number(uid),
+  });
+
+  if (result.serverStatus === 2) {
+    ctx.body = {
+      status: 200,
+      msg: "车位添加成功",
+      data: {
+        pname,
+        comid: Number(comid),
+        x: Number(x),
+        y: Number(y),
+        z: Number(z),
+        direction: Number(direction),
+        uid: Number(uid),
+      },
+    };
+  } else {
+    ctx.body = {
+      status: 0,
+      msg: "车位添加失败",
+    };
+  }
+};
+
+// 用户添加车位
+module.exports.userBindCarport = async (ctx) => {
+  const { pid, uid } = ctx.request.query;
+  const result = await addCarportToUser({ pid, uid });
+  if (result.serverStatus === 2) {
+    ctx.body = {
+      status: 200,
+      msg: "车位绑定成功",
+      data: {
+        pid,
+        uid,
+      },
+    };
+  } else {
+    ctx.body = {
+      status: 0,
+      msg: "车位绑定失败",
+    };
+  }
+};
+
+// 共享车位
+module.exports.rentCarport = async (ctx) => {
+  const { starttime, endtime, comid, pid } = ctx.request.body;
+  const result = await addRentByCarport({
+    starttime: new Date(starttime).toLocaleString().replaceAll("/", "-"),
+    endtime: new Date(endtime).toLocaleString().replaceAll("/", "-"),
+    comid,
+    pid,
+  });
+  if (result.serverStatus === 2) {
+    ctx.body = {
+      status: 200,
+      msg: "车位共享成功",
+      data: {
+        starttime,
+        endtime,
+        comid,
+        pid,
+      },
+    };
+  } else {
+    ctx.body = {
+      status: 0,
+      msg: "车位共享失败",
     };
   }
 };
--- model/carport.js
+++ model/carport.js
@@ -3,21 +3,53 @@ const { query } = require("../db/query");
 // 根据用户Id查询车位信息
 module.exports.getCarportByUserID = async ({ uid }) => {
   return await query(
-    "SELECT c.id,pname,state,comname,place,cid,comid,x,y,z,direction FROM carport AS c,community AS com WHERE c.comid = com.id AND uid = ?",
+    "SELECT c.id,province_id,(SELECT name FROM province WHERE id = province_id) AS province,city_id,(SELECT name FROM province WHERE id = city_id) AS city,area_id,(SELECT name FROM province WHERE id = area_id) AS area,place,comname,pname,state,cid,comid,x,y,z,direction FROM carport AS c,community AS com WHERE c.comid = com.id AND uid = ?",
     [uid]
   );
 };
 // 根据小区Id查询车位信息
 module.exports.getCarportByComID = async ({ comid }) => {
   return await query(
-    "SELECT c.id,pname,state,comname,place,cid,comid,x,y,z,direction FROM carport AS c,community AS com WHERE c.comid = com.id AND comid = ?",
+    "SELECT c.id,province_id,(SELECT name FROM province WHERE id = province_id) AS province,city_id,(SELECT name FROM province WHERE id = city_id) AS city,area_id,(SELECT name FROM province WHERE id = area_id) AS area,place,comname,pname,state,cid,comid,x,y,z,direction FROM carport AS c,community AS com WHERE c.comid = com.id AND comid = ?",
     [comid]
   );
 };
 // 根据用户Id与小区ID查询车位信息
 module.exports.getCarportByUserIDAndComID = async ({ uid, comid }) => {
   return await query(
-    "SELECT c.id,pname,state,comname,place,cid,comid,x,y,z,direction FROM carport AS c,community AS com WHERE c.comid = com.id AND uid = ? AND comid = ?",
+    "SELECT c.id,province_id,(SELECT name FROM province WHERE id = province_id) AS province,city_id,(SELECT name FROM province WHERE id = city_id) AS city,area_id,(SELECT name FROM province WHERE id = area_id) AS area,place,comname,pname,state,cid,comid,x,y,z,direction FROM carport AS c,community AS com WHERE c.comid = com.id AND uid = ? AND comid = ?",
     [uid, comid]
+  );
+};
+// 根据小区ID添加车位
+module.exports.addCarportByComID = async ({
+  pname,
+  comid,
+  x,
+  y,
+  z,
+  direction,
+  uid,
+  state = 0,
+}) => {
+  return await query(
+    "INSERT INTO carport (pname,x,y,z,direction,uid,comid,state) VALUES (?,?,?,?,?,?,?,?)",
+    [pname, x, y, z, direction, uid, comid, state]
+  );
+};
+// 车位添加到指定用户上
+module.exports.addCarportToUser = async ({ uid, pid }) => {
+  return await query("UPDATE carport SET uid = ? WHERE id = ?", [uid, pid]);
+};
+// 添加车位到共享列表
+module.exports.addRentByCarport = async ({
+  starttime,
+  endtime,
+  comid,
+  pid,
+}) => {
+  return await query(
+    "INSERT INTO rentlist (starttime,endtime,comid,pid) VALUES (?,?,?,?)",
+    [starttime, endtime, comid, pid]
   );
 };
