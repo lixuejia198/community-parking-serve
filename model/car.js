4c87a428d0a08b59f6edf719927ff13a47242735
F999999999 <9811@88.com>
2022-04-22 20:01:54 +0800

    新增：车辆操作相关接口

--- model/car.js
+++ model/car.js
@@ -1,23 +1,27 @@
 const { query } = require("../db/query");
 
-// 根据用户Id查询车辆信息
+// 根据用户ID查询车辆信息
 module.exports.getCarByUserID = async ({ uid }) => {
   return await query(
     "SELECT car.id,car.cname,car.uid,carport.id AS pid,carport.pname,carport.x,carport.y,carport.z,carport.direction,carport.state,carport.comid FROM car LEFT JOIN carport ON car.id = carport.cid WHERE car.uid = ?",
     [uid]
   );
 };
-// 根据小区Id查询车位信息
+// 根据小区ID查询车位信息
 module.exports.getCarByComID = async ({ comid }) => {
   return await query(
     "SELECT car.id,car.cname,car.uid,carport.id AS pid,carport.pname,carport.x,carport.y,carport.z,carport.direction,carport.state,carport.comid FROM car LEFT JOIN carport ON car.id = carport.cid WHERE carport.comid = ?",
     [comid]
   );
 };
-// 根据用户Id与小区ID查询车位信息
+// 根据用户ID与小区ID查询车位信息
 module.exports.getCarByUserIDAndComID = async ({ uid, comid }) => {
   return await query(
     "SELECT car.id,car.cname,car.uid,carport.id AS pid,carport.pname,carport.x,carport.y,carport.z,carport.direction,carport.state,carport.comid FROM car LEFT JOIN carport ON car.id = carport.cid WHERE car.uid = ? AND carport.comid = ?",
     [uid, comid]
   );
+};
+// 根据用户ID添加车辆
+module.exports.addCarByUserID = async ({ uid, cname }) => {
+  return await query("INSERT INTO car (uid,cname) VALUES (?,?)", [uid, cname]);
 };
