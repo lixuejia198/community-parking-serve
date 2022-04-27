ff17f36aac574ccca2f8e1655361b8149840241b
lixuejia198 <749134054@qq.com>
2022-04-27 11:50:25 +0800

    修改接口注释

--- model/car.js
+++ model/car.js
@@ -7,14 +7,14 @@ module.exports.getCarByUserID = async ({ uid }) => {
     [uid]
   );
 };
-// 根据小区ID查询车位信息
+// 根据小区ID查询车辆信息
 module.exports.getCarByComID = async ({ comid }) => {
   return await query(
     "SELECT car.id,car.cname,car.color,car.uid,carport.id AS pid,carport.pname,carport.x,carport.y,carport.z,carport.direction,carport.state,carport.comid FROM car LEFT JOIN carport ON car.id = carport.cid WHERE carport.comid = ?",
     [comid]
   );
 };
-// 根据用户ID与小区ID查询车位信息
+// 根据用户ID与小区ID查询车辆信息
 module.exports.getCarByUserIDAndComID = async ({ uid, comid }) => {
   return await query(
     "SELECT car.id,car.cname,car.color,car.uid,carport.id AS pid,carport.pname,carport.x,carport.y,carport.z,carport.direction,carport.state,carport.comid FROM car LEFT JOIN carport ON car.id = carport.cid WHERE car.uid = ? AND carport.comid = ?",
